40fde560bcc9905e61d9567a69a09ffb7f9de652
Augustompa <42446408+Augustompa@users.noreply.github.com>
2024-11-23 01:15:53 +0000

    ajustando cors

--- script.js
+++ script.js
@@ -219,8 +219,10 @@ function insertPizza()
     fetch('https://ominous-meme-rpgg69w5jrvh59q5-5000.app.github.dev/insert',{
         method: 'POST',
         headers: {
+
           'Content-Type': 'application/json',
-          'accept':'application/json'
+          'accept':'application/json',
+          'Access-Control-Allow-Origin': '*' 
         },
         body: JSON.stringify({
             Nome: 'teste',
